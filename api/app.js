a5e769d2ef2d8ee1cb75cdc279dddede3f68a4c7
Philbert Lou <philbertlou@live.ca>
2021-09-19 00:01:19 -0400

    Added endpoint

--- api/app.js
+++ api/app.js
@@ -1,15 +1,18 @@
-const express = require('express');
-const app = express();
-const port = 4000;
+const express = require('express')
+const app = express()
+const port = 4000
 
+app.use(express.json());
 app.get('/', (req, res) => {
   res.send('Hello World!')
-});
+})
 
-app.get('/getQuestions', async (req, res) =>{
-
-});
+app.post('/getQuestions', async (req, res) =>{
+    const info = req.body.info
+    res.status(200).send({message: info})
+    return
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
-});
+})
